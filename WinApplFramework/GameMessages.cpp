54f10bcd027559b7cd48498e54d4300d8360bce1
kwalker93 <kwalker93@student.alamo.edu>
2016-05-05 19:15:30 -0500

    kcw
    updated controls message box with proper controls

--- WinApplFramework/GameMessages.cpp
+++ WinApplFramework/GameMessages.cpp
@@ -56,13 +56,16 @@ int GameMessages::controlsMessageBox()
 
    string line3 = "1. Click on one of your squirrels to select them.\n";
    string line4 = "2. Use the arrow keys to move the selected unit on the board. Be mindful of the Info page, since each squirrel has a limited movement range.\n";
-   string line5 = "3. When your selected squirrel is within attacking range of the other player's squirrels, you can damage them.\n";
-   string line6 = "4. Once a squirrel's health goes to 0, they're toast, so be careful!\n\n";
-   string line7 = "The last player with squirrels remaining wins the game!\n\n\n\n";
+   string line5 = "3. After you've moved your selected squirrel to where you want, hit the 'ENTER' key to enter your attack phase.\n";
+   string line6 = "4. While in the attack phase, a red cursor can be moved around your selected squirrel with the arrow keys.\n"; 
+   string line7 = "5. When your attack cursor is highlighting an enemy squirrel, you can then press the 'SHIFT' key to damage them. If no squirrel is highlighted, pressing 'SHIFT' will simply end your turn.\n";   
 
-   string line8 = "Oh, by the way...\n\nYou better win quickly, or else the nearby river might engulf you with its daily flooding!\n";
+   string line8 = "7. Once a squirrel's health goes to 0, they're toast, so be careful!\n\n";
+   string line9 = "The last player with squirrels remaining wins the game!\n\n\n\n";
+
+   string line10 = "Oh, by the way...\n\nYou better win quickly, or else the nearby river might engulf you with its daily flooding!\n";
    
-   string concatString = line0 + line1 + line2 + line3 + line4 + line5 + line6 + line7 + line8;
+   string concatString = line0 + line1 + line2 + line3 + line4 + line5 + line6 + line7 + line8 + line9 + line10;
 
    return MessageBox(NULL, concatString.c_str(), "Controls", MB_YESNO | MB_ICONEXCLAMATION );
 }
